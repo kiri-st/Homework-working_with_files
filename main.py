3c8a77312543e794692a6c81c606ece3d45d0163
kiri_st <kiri_s.20-91x2@mail.ru>
2023-12-11 23:09:30 +0500

    task_2:completed

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# /task_1///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+from pprint import pprint
 with open('recipes.txt', 'r', encoding='utf-8') as f:
     file = f.read().split('\n')
 
@@ -19,5 +21,18 @@ with open('recipes.txt', 'r', encoding='utf-8') as f:
             product_list.append(product_dict)
             cook_book[text[0]] = product_list
         product_list = []
+    pprint(cook_book)
 
-    print(cook_book)
+# /task_2///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+def get_shop_list_by_dishes(dishes, person_count):
+    must_have_products = {}
+    dishes_list = cook_book.keys()
+    for dish in dishes:
+        if dish in dishes_list:
+            products = cook_book[dish]
+            for product in products:
+                product_count = int(product.get('quantity')) * person_count
+                must_have_products[product.get('ingredient_name')] = {'measure': product.get('measure'),
+                                                                      'quantity': product_count}
+    return must_have_products
+pprint(get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2))
