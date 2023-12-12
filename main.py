ceeeca85fcdf6b0be6e3165a42e853fdf594ce12
kiri_st <kiri_s.20-91x2@mail.ru>
2023-12-12 22:49:31 +0500

    task_3:completed

--- main.py
+++ main.py
@@ -36,3 +36,25 @@ def get_shop_list_by_dishes(dishes, person_count):
                                                                       'quantity': product_count}
     return must_have_products
 pprint(get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2))
+
+# /task_3///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+with (open('1.txt', 'r', encoding='utf-8') as txt_1,
+      open('2.txt', 'r', encoding='utf-8') as txt_2,
+      open('3.txt', 'r', encoding='utf-8') as txt_3,
+      open('completed_text_for_task_3.txt', 'w', encoding='utf-8') as completed_text):
+
+    all_text_dict = ({'1.txt': txt_1.readlines(),
+                     '2.txt': txt_2.readlines(),
+                     '3.txt': txt_3.readlines()})
+
+    sorting_str = dict(sorted(all_text_dict.items(), key=lambda item: item[1], reverse=True))
+
+    for str_ in sorting_str.items():
+        completed_text.write(f'{str_[0]}\n')
+        completed_text.write(f'{len(str_[1])}\n')
+        completed_text.write(f'\n')
+        for line in str_[1]:
+            if '\n' in line:
+                completed_text.write(line)
+            else:
+                completed_text.write(f'{line}\n\n')
